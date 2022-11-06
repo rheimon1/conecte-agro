4d79f6dbed88bf21d324a9648624805680acc647
Wallace <wallacerheimon@gmail.com>
2022-11-06 19:30:07 -0300

    adicionar sidebar

---
+++ src/pages/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/common/Header";
+
+const User = () => {
+	return (
+		<main>
+			<Header />
+			<div id="sidebar"></div>
+		</main>
+	);
+};
+
+export default User;
